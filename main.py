5076e23bddf34df510bea5b3e0e5c0abe178bca9
Johnathan Davis <jdavis4329@gmail.com>
2022-07-20 20:53:13 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+x = 10
+
+def x_changer(new_value):
+    x = new_value
+    return x
+
+print(x_changer(14689))
+
+print(x)
+
+
+hexagram_index = {1: "Creative Power", 2: "Natural Response", 3: "Difficult Beginnings"}
+print(hexagram_index[2])
